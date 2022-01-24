e9ac05e0a80135fe01370bf65c8a6dea5d6812ae
Giorgio <giorgio.cesaroni@me.com>
2022-01-24 14:08:57 +0100

    Imported Athelas from CDN

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,6 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Header({ isTrayOpen, setTrayOpen }) {
-
   function toggleMenu() {
     setTrayOpen(!isTrayOpen);
   }
@@ -16,12 +15,19 @@ export default function Header({ isTrayOpen, setTrayOpen }) {
           <title>Colle Juno</title>
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+          <meta
+            name="viewport"
+            content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+          />
           <meta name="description" content="Il sito di Colle Juno" />
           <meta name="theme-color" content="#008000" />
           <link rel="manifest" href="manifest.json" />
           <link rel="shortcut icon" href="favicon.ico" />
           <link rel="apple-touch-icon" href="apple-icon.png"></link>
+          <link
+            href="http://fonts.cdnfonts.com/css/athelas"
+            rel="stylesheet"
+          ></link>
         </Head>
         <div className="header-content">
           <Link href="/">
@@ -34,7 +40,10 @@ export default function Header({ isTrayOpen, setTrayOpen }) {
           />
         </div>
       </header>
-      <div onClick={toggleMenu} className={"tray" + (!isTrayOpen ? " hidden" : "")}>
+      <div
+        onClick={toggleMenu}
+        className={"tray" + (!isTrayOpen ? " hidden" : "")}
+      >
         <div className="menu">
           <Link href="/products">Prodotti</Link>
           <Link href="/activities">Attività</Link>
